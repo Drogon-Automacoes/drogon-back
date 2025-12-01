4bb24df39a366dc2ae3f69f679de90b165e97faa
murilopbs <murilopbsouto@gmail.com>
2025-12-01 18:32:18 -0300

    Arrumando super user no render

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Criando Super Admin..."
-python scripts/create_super_user.py
+set -e
 
-echo "Iniciando Servidor..."
-fastapi run app/main.py --port 8000 --host 0.0.0.0
+echo "Iniciando o deploy no render"
+
+echo "Verificando Super Admin..."
+python scripts/create_super_user.py || echo "Aviso: Script de criação retornou erro (talvez usuário já exista). Seguindo..."
+
+echo "Subindo o Servidor Uvicorn..."
+exec fastapi run app/main.py --port 8000 --host 0.0.0.0
